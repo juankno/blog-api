84be01d33a39a8f12e480c42e85e8afa325a921e
Juan Esteban <juanes0202@gmail.com>
2022-02-12 12:01:44 -0500

    cambiando limite de paginacion

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return new PostCollection(Post::with('author', 'comments')->paginate(1));
+        return new PostCollection(Post::with('author', 'comments')->paginate(10));
     }
 
     /**
